c86ad1a6240aca8cdb67d84642c4d6247b35c3a4
Robinson Santos - Engenharia - Inobram Automações <robinsonsantos@localhost.localdomain>
2020-09-10 10:46:07 -0300

    change fields mac and at to device_id and temperature

--- api/api/reading/models.py
+++ api/api/reading/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Reading(models.Model):
-    mac = models.CharField(max_length=17, blank=False, null=False, db_index=True)
-    at = models.FloatField(blank=True, null=True)
+    device_id = models.CharField(max_length=50, 
+                                 blank=False, 
+                                 null=False, 
+                                 db_index=True, 
+                                 default=0)
+    temperature = models.FloatField(blank=True, null=True)
     rec_at = models.CharField(max_length=19, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)    
